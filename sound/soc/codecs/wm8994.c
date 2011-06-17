f6cbc148f12cd3d8be4bd36e796c3d2ec6ff450d
SIMOND François <francois@lienweb.fr>
2011-06-17 16:09:36 +0200

    Voodoo sound: driver v9 beta for Sidekick 4G

--- sound/soc/codecs/wm8994.c
+++ sound/soc/codecs/wm8994.c
@@ -48,6 +48,11 @@
 #else
 #include "HAC.h"
 #endif
+
+#ifdef CONFIG_SND_VOODOO
+#include "wm8994_voodoo.h"
+#endif
+
 #define WM8994_VERSION "0.1"
 #define SUBJECT "wm8994.c"
 
@@ -195,6 +200,10 @@ int wm8994_write(struct snd_soc_codec *codec, unsigned int reg, unsigned int val
 	u8 data[4];
 	int ret;
 	//BUG_ON(reg > WM8993_MAX_REGISTER);
+
+#ifdef CONFIG_SND_VOODOO
+	value = voodoo_hook_wm8994_write(codec, reg, value);
+#endif
 
 	/* data is
 	 *   D15..D9 WM8993 register offset
@@ -2139,6 +2148,11 @@ static int wm8994_pcm_probe(struct platform_device *pdev)
 #else
                 /* Add other interfaces here */
 #endif
+
+#ifdef CONFIG_SND_VOODOO
+	voodoo_hook_wm8994_pcm_probe(codec);
+#endif
+
         return ret;
 }
 
